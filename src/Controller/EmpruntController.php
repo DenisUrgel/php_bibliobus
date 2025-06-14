b4632e9b67aafd3aa8c7dc5243790c6f7e86f645
Denis <denisurgel04@gmail.com>
2025-06-14 21:25:44 +0200

    erreur bookController

--- src/Controller/EmpruntController.php
+++ src/Controller/EmpruntController.php
@@ -5,24 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\LivreRepository;
+use App\Repository\BookRepository;
 use App\Repository\EmpruntRepository;
-use App\Entity\Livre;
+use App\Entity\Book;
 
 class EmpruntController extends AbstractController
 {
     #[Route('/livres-disponibles', name: 'livres_disponibles')]
-    public function livresDisponibles(EmpruntRepository $empruntRepository, LivreRepository $livreRepository): Response
+    public function livresDisponibles(EmpruntRepository $empruntRepository, BookRepository $livreRepository): Response
     {
-        $livres = $livreRepository->findLivresDisponibles();
+        $livres = $livreRepository->findAll();
 
-        return $this->render('user/livres_disponibles.html.twig', [
+        return $this->render('user/book/livres_disponibles.html.twig', [
             'livres' => $livres,
         ]);
     }
 
     #[Route('/reserver-livre/{id}', name: 'reserver_livre')]
-    public function reserverLivre(Livre $livre): Response
+    public function reserverLivre(Book $book): Response
     {
         // Logique de réservation à faire plus tard
         return $this->redirectToRoute('livres_disponibles');
--- src/Controller/User/Book/BookController.php
+++ src/Controller/User/Book/BookController.php
@@ -14,19 +14,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/user')]
 final class BookController extends AbstractController
 {
-    #[Route('/book-list', name: 'app_book_list', methods:['GET'])]
+    #[Route('/book-list', name: 'app_book_list', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        $bookList = $entityManager->getRepository(Book::class)->findAll();
+        // $bookList = $entityManager->getRepository(Book::class)->findAll();
+
+        $qb = $entityManager->createQueryBuilder();
+
+        $qb->select('b')
+            ->from(Book::class, 'b')
+            ->leftJoin('b.emprunt_book', 'eb')
+            ->leftJoin('eb.emprunt', 'e')
+            ->where('e.id IS NULL OR e.status = 0')
+            ->groupBy('b.id');
+
+        $bookListNonEmprunter = $qb->getQuery()->getResult();
 
         return $this->render('/user/book/index.html.twig', [
             'controller_name' => 'BookController',
-            'bookList' => $bookList
+            'bookList' => $bookListNonEmprunter
         ]);
     }
 
     #[Route('/book-details/{id}', name: 'app_book_details', methods: ['GET'])]
-    public function bookDetails(EntityManagerInterface $entityManager, int $id): Response{
+    public function bookDetails(EntityManagerInterface $entityManager, int $id): Response
+    {
 
         $book = $entityManager->getRepository(Book::class)->find($id);
         $auteur = $entityManager->getRepository(Author::class)->find($book->getAuteurId());
@@ -39,12 +51,9 @@ final class BookController extends AbstractController
             'bookCollection' => $collection->getName(),
         ]);
     }
-}
 
-class BookController extends AbstractController
-{
-    #[Route('/bookListNonEmprunter', name: 'app_book_list_non_emprunter', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route('/livres-disponibles', name: 'app_book_list_non_emprunter', methods: ['GET'])]
+    public function livresDisponibles(EntityManagerInterface $entityManager): Response
     {
         $qb = $entityManager->createQueryBuilder();
 
@@ -57,7 +66,7 @@ class BookController extends AbstractController
 
         $bookListNonEmprunter = $qb->getQuery()->getResult();
 
-        return $this->render('user/book/index.html.twig', [
+        return $this->render('user/book/livres_disponibles.html.twig', [
             'bookListNonEmprunter' => $bookListNonEmprunter,
         ]);
     }
