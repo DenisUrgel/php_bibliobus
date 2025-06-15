b4d2d2833957c2893f2dc0d7f72e11926e299a4d
Denis <denisurgel04@gmail.com>
2025-06-15 15:55:14 +0200

    ajout emprunt

--- src/Controller/Admin/Book/BookController.php
+++ src/Controller/Admin/Book/BookController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin\Book;
 
 use App\Entity\Book;
 use App\Entity\Author;
+use App\Entity\Emprunt;
 use App\Entity\Collections;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,6 +37,32 @@ final class BookController extends AbstractController
             'book' => $book,
             'bookAuthor' => $auteur,
             'bookCollection' => $collection->getName(),
+        ]);
+    }
+
+    #[Route('/emprunt', name: 'app_admin_emprunt', methods: ['GET'])]
+    public function emprunt(EntityManagerInterface $entityManager): Response
+    {
+        $emprunts = $entityManager->getRepository(Emprunt::class)->findAll();
+
+        return $this->render('/admin/book/emprunt.html.twig', [
+            'controller_name' => 'BookController',
+            'emprunts' => $emprunts
+        ]);
+    }
+
+    #[Route('/rendu/{id}', name: 'app_admin_rendu', methods: ['GET'])]
+    public function rendu(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $emprunts = $entityManager->getRepository(Emprunt::class)->findAll();
+        $emprunt = $entityManager->getRepository(Emprunt::class)->find($id);
+        $emprunt->setStatus('0');
+        $entityManager->persist($emprunt);
+        $entityManager->flush();
+
+        return $this->render('/admin/book/emprunt.html.twig', [
+            'controller_name' => 'BookController',
+            'emprunts' => $emprunts
         ]);
     }
 }
--- src/Controller/User/Book/BookController.php
+++ src/Controller/User/Book/BookController.php
@@ -3,11 +3,16 @@
 namespace App\Controller\User\Book;
 
 use App\Entity\Book;
+use App\Entity\User;
 use App\Entity\Author;
+use DateTimeImmutable;
+use App\Entity\Emprunt;
 use App\Entity\Collections;
 use Doctrine\ORM\EntityManagerInterface;
+use function Symfony\Component\Clock\now;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -17,22 +22,28 @@ final class BookController extends AbstractController
     #[Route('/book-list', name: 'app_book_list', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        // $bookList = $entityManager->getRepository(Book::class)->findAll();
+        $bookList = $entityManager->getRepository(Book::class)->findAll();
 
-        $qb = $entityManager->createQueryBuilder();
+        $bookAvailable = [];
 
-        $qb->select('b')
-            ->from(Book::class, 'b')
-            ->leftJoin('b.emprunt_book', 'eb')
-            ->leftJoin('eb.emprunt', 'e')
-            ->where('e.id IS NULL OR e.status = 0')
-            ->groupBy('b.id');
-
-        $bookListNonEmprunter = $qb->getQuery()->getResult();
+        foreach ($bookList as $book) {
+            $emprunts = $book->getEmprunts()->toArray();
+            if (empty($emprunts)) {
+                $bookAvailable[] = $book;
+            } else {
+                usort($emprunts, function ($a, $b) {
+                    return $b->getBorrowedAt() <=> $a->getBorrowedAt();
+                });
+                $dernierEmprunt = $emprunts[0];
+                if ($dernierEmprunt->getStatus() === '0' || $dernierEmprunt->getStatus() === 0) {
+                    $bookAvailable[] = $book;
+                }
+            }
+        }
 
         return $this->render('/user/book/index.html.twig', [
             'controller_name' => 'BookController',
-            'bookList' => $bookListNonEmprunter
+            'bookList' => $bookAvailable
         ]);
     }
 
@@ -52,22 +63,33 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/livres-disponibles', name: 'app_book_list_non_emprunter', methods: ['GET'])]
-    public function livresDisponibles(EntityManagerInterface $entityManager): Response
+    #[Route('/book-reserve/{id}/{user_id}', name: 'app_book_reserve', methods: ['GET'])]
+    public function bookResever(EntityManagerInterface $entityManager, int $id, int $user_id): Response
     {
-        $qb = $entityManager->createQueryBuilder();
+        $user = $entityManager->getRepository(User::class)->find($user_id);
 
-        $qb->select('b')
-            ->from(Book::class, 'b')
-            ->leftJoin('b.empruntBooks', 'eb')
-            ->leftJoin('eb.emprunt', 'e')
-            ->where('e.id IS NULL OR e.status = 0')
-            ->groupBy('b.id');
+        $book = $entityManager->getRepository(Book::class)->find($id);
+        $auteur = $entityManager->getRepository(Author::class)->find($book->getAuteurId());
+        $collection = $entityManager->getRepository(Collections::class)->find($book->getId());
 
-        $bookListNonEmprunter = $qb->getQuery()->getResult();
+        $today = new DateTimeImmutable();
 
-        return $this->render('user/book/livres_disponibles.html.twig', [
-            'bookListNonEmprunter' => $bookListNonEmprunter,
+        $emprunt = new Emprunt();
+        $emprunt->setUser($user);
+        $emprunt->setBorrowedAt($today);
+        $emprunt->setMustBeReturnedAt($today->modify('+3 months'));
+        $emprunt->setStatus('1');
+        $emprunt->addBook($book);
+        
+        $entityManager->persist($emprunt);
+        $entityManager->flush();
+
+        return $this->render('/user/book/details.html.twig', [
+            'controller_name' => 'BookController',
+            'book' => $book,
+            'bookAuthor' => $auteur,
+            'bookCollection' => $collection->getName(),
+            'message' => 'Livre ' . $book->getTitle() . ' bien réservé par ' . $user->getFirstName(),
         ]);
     }
 }
